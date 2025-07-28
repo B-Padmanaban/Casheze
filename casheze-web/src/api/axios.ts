e32e0650851a6c705d5ec983cda25263d0e488a1
Padmanaban_B <padmanabanbalaji01@gmail.com>
2025-07-28 20:49:49 +0530

    Added Env file in Web

--- casheze-web/src/api/axios.ts
+++ casheze-web/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Replace with live URL if hosted
+  baseURL: process.env.REACT_APP_API_BASE_URL, // Reads from .env file
   headers: {
     'Content-Type': 'application/json',
   },
