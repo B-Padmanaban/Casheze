93d577bf6000e42a174ac76751ab1c9b018c3ab9
Padmanaban_B <padmanabanbalaji01@gmail.com>
2025-07-28 21:24:34 +0530

    Axios Changes

--- casheze-web/src/api/axios.ts
+++ casheze-web/src/api/axios.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-
-dotenv.config(); // Load .env variables
 
 const axiosInstance = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
